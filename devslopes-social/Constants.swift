27d0e03cdab07cbc89038d9370fc6496074962b7
manytime777 <manytime@777.so-net.jp>
2016-10-05 01:39:23 +0900

    Sign in ui custom classes complete

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by 松岡泰史 on 2016/10/05.
+//  Copyright © 2016年 松岡泰史. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255
---
+++ devslopes-social/RoundBtn.swift
@@ -0,0 +1,31 @@
+//
+//  RoundBtn.swift
+//  devslopes-social
+//
+//  Created by 松岡泰史 on 2016/10/05.
+//  Copyright © 2016年 松岡泰史. All rights reserved.
+//
+
+import UIKit
+
+class RoundBtn: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        imageView?.contentMode = .scaleAspectFit
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
+    }
+    
+    
+
+}
